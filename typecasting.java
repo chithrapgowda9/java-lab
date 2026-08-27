201a200a1f2c8a12f2bbf2053aa43964989c1a6e
chithrapgowda9 <chithrapgowda9@gmail.com>
2026-08-27 11:49:17 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,11 @@
+public class typecasting {
+    public static void main(String[] args) {
+        double x = 30.75;
+        int y;
+
+        y = (int) x;
+
+        System.out.println("Double value: " + x);
+        System.out.println("Integer value: " + y);
+    }
+}
